3ed82d8c12130a4dbaab523347cd439d61eda393
Rhem2022 <reyax2006@gmail.com>
2023-03-21 17:57:32 +0000

    map-to-country

--- main.tf
+++ main.tf
@@ -1,16 +1,21 @@
-variable "names" {
-  default = ["ade", "ravs", "tom", "jane"]
-  #default = ["ranga", "tom", "jane"]
+variable "users" {
+  default = {
+    ade : "Niger"
+    ravs : "us"
+    tom : "India"
+  }
+
 }
 
 provider "aws" {
   region = "us-west-1"
- // version = "~> 2.46"
+  //version = "~> 2.46" 
 }
 
 resource "aws_iam_user" "my_iam_users" {
-  #count = length(var.names)
-  #name  = var.names[count.index]
-  for_each = toset(var.names)
-  name     = each.value
+    for_each = var.users
+      name = each.key
+      tags = {
+        country: each.value
+      }
 }
